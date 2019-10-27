66a0e60a59ae48490cbebac40a342f3e170eef23
AhmetHan <han.ahmet@icloud.com>
2019-10-27 23:16:33 +0300

    Update EDR-Tester.bat

--- EDR-Tester.bat
+++ EDR-Tester.bat
@@ -243,6 +243,8 @@ netsh interface show
 ECHO ============================
 getmac
 ECHO ============================
+tasklist /v
+ECHO ============================
 ping -n 10 127.0.0.1
 ECHO ============================
 nslookup bcdyzit4r3e5tet6y3e6y3w3e6y6y6y.testdeneme12345edced.com
