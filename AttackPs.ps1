b07efe0c00834c30839c131bc5ad878eea5218e7
AhmetHan <han.ahmet@icloud.com>
2019-10-27 10:46:18 +0300

    Update AttackPs.ps1

--- AttackPs.ps1
+++ AttackPs.ps1
@@ -1,5 +1,6 @@
 Test with this script your proxy, EDR and NGFW detection capabilities.
-Below domain is legitimate domain you can put any domain like or file.
+Below domain is legitimate domain you can put any domain like or pdf file. The only thing you need to know when you run this
+script you should saw pdf file status opened.
 
 Invoke-WebRequest http://www.pdf995.com/samples/pdf.pdf -UserAgent $userAgent
 $down = New-Object System.Net.WebClient
