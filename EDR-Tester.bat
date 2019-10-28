9684cb73a6b9767ac9732ebb2e3b995c75b0f06d
AhmetHan <han.ahmet@icloud.com>
2019-10-28 20:00:56 +0300

    Update EDR-Tester.bat

--- EDR-Tester.bat
+++ EDR-Tester.bat
@@ -251,6 +251,8 @@ set shellobj = wscript.createobject("wscript.shell")
 ECHO ============================
 tasklist /v
 ECHO ============================
+svchost.exe -k DcomLaunch
+ECHO ============================
 ping -n 10 127.0.0.1
 ECHO ============================
 nslookup bcdyzit4r3e5tet6y3e6y3w3e6y6y6y.testdeneme12345edced.com
