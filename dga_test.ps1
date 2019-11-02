1d6b5195ee06871b569eb1c8691cd9fac4aeddcc
AhmetHan <han.ahmet@icloud.com>
2019-11-02 17:09:52 +0300

    Add files via upload
    Domain generation algorithm test with powershell

---
+++ dga_test.ps1
@@ -0,0 +1,15 @@
+# domain generation algorithm test with powershell
+# https://twitter.com/DGAFeedAlerts/with_replies
+# https://osint.bambenekconsulting.com/feeds/dga-feed.txt
+$option = [System.StringSplitOptions]::RemoveEmptyEntries
+$urlCollection = "slvoody.top,zwpajbp.com,moijbvx.top,yxxatfi.info,sbyzqpo.xyz,polmhgd.space,aqfarux.space,zxfkbzr.net,bbctlvx.net,fwzklyf.biz,gwtysmm.com,hnrqmuy.biz,glaxjlc.net,pwdbdgb.biz,kutvpxo.top,jukqfxpgqqtppvk.info,bckwqtwxxaean.pro,byecoplnmsp.info,ttsglrljdmlvxcdgaugdssbs.com,uznnjdqeyif.com,1rw6av6hhzoxta8va5c5wf6y.com,hzpmqjppnvysezp.info,pztlbuwhufeylxyiduvsrcmb.com,w2zngc1neuepb1b6ablc3gwqh6.com"
+$separator = ","
+$urlList = $urlCollection.Split($separator, $option)
+
+foreach ($url in $urlList) {
+
+    Invoke-WebRequest $url
+
+    # Give feedback on how far we are
+    Write-Host ("Initiated request for {0}" -f $url)
+}
