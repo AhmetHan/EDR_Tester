c24c425d1be93e74aefbad630b510be785b0bdeb
AhmetHan <han.ahmet@icloud.com>
2019-11-09 10:59:28 +0300

    Update AttackPs.ps1

--- AttackPs.ps1
+++ AttackPs.ps1
@@ -1,6 +1,6 @@
 Test with this script your proxy, EDR and NGFW detection capabilities.
 Below domain is legitimate domain you can put any domain like or pdf file. The only thing you need to know when you run this
-script check your edr, proxy and firewall and responses.
+script check your edr, proxy and firewall and responses. Then you should check SIEM events.
 
 Invoke-WebRequest http://www.pdf995.com/samples/pdf.pdf -UserAgent $userAgent
 $down = New-Object System.Net.WebClient
